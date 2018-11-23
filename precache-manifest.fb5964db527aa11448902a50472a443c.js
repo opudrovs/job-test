3ee54ac39808d7fac74e9053521eaa3ba81e785f
Olga Pudrovska <opudrovs@gmail.com>
2018-11-23 20:58:00 +0200

    Updates

---
+++ precache-manifest.fb5964db527aa11448902a50472a443c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64f73b682da8cf82792c",
+    "url": "/job-test/static/js/main.64f73b68.chunk.js"
+  },
+  {
+    "revision": "b25095fb461ad9824604",
+    "url": "/job-test/static/js/1.b25095fb.chunk.js"
+  },
+  {
+    "revision": "9c7568f40512b961e47a",
+    "url": "/job-test/static/js/runtime~main.9c7568f4.js"
+  },
+  {
+    "revision": "e700bab79e308accf0760f3d0dc3e178",
+    "url": "/job-test/static/media/logo.e700bab7.svg"
+  },
+  {
+    "revision": "eb13a0ddd6a456518e705ecd37b95d58",
+    "url": "/job-test/index.html"
+  }
+];
