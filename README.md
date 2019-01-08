6f8a2bf8cccdb9963c33b16f3b9c39b8f5ea7a58
Olga Pudrovska <opudrovs@gmail.com>
2019-01-08 03:56:12 +0200

    Added more info to Readme.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # job-test
 
+The project demonstrates the usage of React.js for loading and presenting user data from Randomuser.me REST API and searching for users by name.
+
+The app's UI is responsive (please try resizing browser window and clicking on users under different screen sizes).
+
+The project uses Node.js, Webpack and CSS Modules.
+
 [Demo](https://opudrovs.github.io/job-test/)
---
+++ src/components/pages/PageHome/UserInfoPopup/UserInfoPopup.js
@@ -0,0 +1,75 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import InfoItem from './InfoItem/InfoItem';
+
+import { composeFullName, composeAddress } from 'utils/UtilsUser';
+import { sanitizeString } from 'utils/UtilsString';
+
+import styles from './UserInfo.css';
+
+const UserInfoPopup = ({ user }) => {
+    const fullName = composeFullName(user);
+
+    const items = user
+        ?
+        [
+            { label: 'Name', text: fullName, style: { textTransform: 'capitalize' }, classNames: [styles.itemName] },
+            { label: 'Email Address', text: sanitizeString(user.email) },
+            { label: 'Phone Number', text: sanitizeString(user.cell) },
+            { label: 'Address', text: composeAddress(user), style: { textTransform: 'capitalize' } },
+        ]
+        :
+        [];
+
+    const userHasLargePicture = user && user.picture && user.picture.large;
+    const largePictureUrl = userHasLargePicture ? user.picture.large : '' ;
+    const backgroundDivStyle = {};
+
+    if (userHasLargePicture) {
+        backgroundDivStyle.backgroundImage = `url(${largePictureUrl})`;
+    }
+
+    return (
+        <div className={`${styles.userInfo}${user ? '' : ` ${styles.hidden}`}`}>
+            <div className={styles.scrollable}>
+                {user
+                &&
+                <Fragment>
+                    <div className={styles.portraitContainer}>
+                        <div className={styles.portraitBackground}
+                             style={backgroundDivStyle}
+                        />
+                        {userHasLargePicture
+                        &&
+                        <img
+                            src={largePictureUrl}
+                            title={fullName}
+                            alt={fullName}
+                            className={styles.portrait}
+                        />}
+                        <span className={styles.name}>{fullName}</span>
+                        <span className={styles.additionalInfo}>{user.cell}</span>
+                    </div>
+                    <div className={styles.itemsContainer}>
+                        {items.map((element, index) =>
+                            <InfoItem
+                                key={`item ${index}`}
+                                label={element.label}
+                                text={element.text}
+                                style={element.style}
+                                classNames={element.classNames}
+                            />
+                        )}
+                    </div>
+                </Fragment>}
+            </div>
+        </div>
+    );
+};
+
+export default UserInfo;
+
+UserInfo.propTypes = {
+    user: PropTypes.object
+};
